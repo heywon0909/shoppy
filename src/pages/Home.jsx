5b37db1624bca817fc31771052adbe636d79597d
hw7319 <hw7319@naver.com>
2023-03-30 17:48:09 +0900

    메인 이미지 삽입

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div className="w-full">
+      <div>
+        <img src="../assets/image/main.jpg" alt="mainimage" />
+      </div>
+    </div>
+  );
 }
