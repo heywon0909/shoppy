99b0a7f5e9a8052fe466cadce61992f42c1c3e19
heywon0909 <hw7319@naver.com>
2023-03-31 18:28:53 +0900

    header 작업중..

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import { BsCartCheck } from "react-icons/bs";
 import { FaUserCircle } from "react-icons/fa";
 export default function Header() {
   return (
-    <div className="w-full flex p-2 items-stretch">
-      <div className="text-2xl font-mono grow">SHOPPY</div>
-      <div className="flex grow flex-row-reverse p-2">
-        <button>
+    <div className="w-full flex p-2 items-stretch fixed">
+      <div className="text-2xl grow flex items-center">SHOPPY</div>
+      <div className="flex grow flex-row-reverse p-2 mr-5">
+        <button className='ml-2'>
           <FaUserCircle size="25" />
         </button>
         <button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="w-full">
-      <div>
-        <img src="../assets/image/main.jpg" alt="mainimage" />
+    <div className="w-full h-full">
+      <div className="xl:h-3/5 md:h-4/5 h-5/6 w-full relative">
+        <img src="../assets/image/main.jpg" alt="mainimage" className="w-full h-full" />
+        <div className="absolute w-full h-auto text-center xl:top-28 top-16">
+          <p className='xl:text-4xl text-2xl text-zinc-900'>our</p>
+          <p className='xl:text-7xl text-4xl text-zinc-900'>MID-SEASON</p>
+           <p className='xl:text-7xl text-4xl text-zinc-900'>SALE</p>
+        </div>
       </div>
     </div>
   );
