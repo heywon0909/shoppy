9127b0076469fd77ac78e0c5c7fdcb4022ff5607
hw7319 <hw7319@naver.com>
2023-04-13 14:46:54 +0900

    구글 로그인 팝업 대신 리다이렉트로 구현

--- src/components/LoginStateBar.jsx
+++ src/components/LoginStateBar.jsx
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import CryptoJS from "crypto-js";
 // import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { useLoginApi } from "../context/LoginContext";
 import { useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import { getLoginApply, getLoginDismiss } from "../api/ShopServices";
+import { getAuth, GoogleAuthProvider, getRedirectResult } from "firebase/auth";
+import {
+  getLoginApply,
+  getLoginDismiss,
+  signWithGoogle,
+} from "../api/ShopServices";
 export default function LoginStateBar() {
   const { login, setValidateUser } = useLoginApi();
   const navigate = useNavigate();
 
-  const { refetch: loginApply, data: user } = useQuery(
-    ["login"],
-    getLoginApply,
-    {
-      enabled: false,
-      onSuccess: (data) => {
-        console.log('data',data)
-        setValidateUser(data);
-      },
+  const {
+    isLoading: isLoginApplying,
+    refetch: loginApply,
+    data: user,
+  } = useQuery(["login"], signWithGoogle, {
+    enabled: false,
+  });
+
+  const getGoogleOAuthResponse = async () => {
+    const auth = getAuth();
+    getRedirectResult(auth).then((result) => {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      // const credential = GoogleAuthProvider.credentialFromResult(result);
+      // const token = credential.accessToken;
+
+      // The signed-in user info.
+      const user = result.user;
+      console.log("user", user);
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+      setValidateUser(user);
+    });
+  };
+
+  useEffect(() => {
+    if (isLoginApplying) {
+      getGoogleOAuthResponse();
     }
-  );
+  }, [isLoginApplying]);
 
   const { refetch: logoutApply } = useQuery(["logout"], getLoginDismiss, {
     enabled: false,
@@ -51,7 +74,7 @@ export default function LoginStateBar() {
       </button>
 
       <button className="mr-2 text-xs text-slate-300" onClick={handleLogin}>
-        {user ? "로그아웃" : "로그인"}
+        {user || sessionStorage.login ? "로그아웃" : "로그인"}
       </button>
     </div>
   );
