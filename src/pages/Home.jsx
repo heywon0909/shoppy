f2d8db75e9e058ea78367e1a3e4272ba71a4d8de
heywon0909 <hw7319@naver.com>
2023-04-13 21:53:01 +0900

    메인 배너 이미지 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,22 +3,21 @@ import Items from "../components/Items";
 export default function Home() {
   return (
     <div className="w-full h-screen flex flex-col" id="grid">
-      <div className="w-full h-1/3 relative">
+      <div className="w-full h-4/6 relative">
         <img
           src="../assets/image/main.jpg"
           alt="mainimage"
           className="w-full h-full"
         />
-        <div className="absolute w-full h-full text-center top-0 flex flex-col justify-center items-center space-y-2">
-          <div>
-            <p className="xl:text-3xl md:text-2xl text-xl text-zinc-50">our</p>
-            <p className="xl:text-5xl md:text-4xl text-3xl text-zinc-50">
+        <div className="absolute w-full h-full text-center top-0 flex flex-col justify-center items-center xl:space-y-9">
+            <p className="xl:text-5xl md:text-2xl text-xl text-green-900 font-mono">our</p>
+            <p className="xl:text-6xl md:text-4xl text-3xl text-green-900 font-mono tracking-wide">
               MID-SEASON
             </p>
-            <p className="xl:text-5xl md:text-4xl text-3xl text-zinc-50">
+            <p className="xl:text-7xl md:text-4xl text-4xl text-white font-mono">
               SALE
             </p>
-          </div>
+        
         </div>
       </div>
       <div className="w-full flex justify-center">
