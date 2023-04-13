2d12e762e86bb0d498d2fd3f8d815e99e3329f10
hw7319 <hw7319@naver.com>
2023-04-13 14:47:18 +0900

    react-toastify 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Root from "./pages/Root";
 import NotFoundPage from "./pages/NotFoundPage";
 import Home from "./pages/Home";
 import Item from "./pages/Item";
 import MyPage from "./pages/MyPage";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -15,12 +18,18 @@ const router = createBrowserRouter([
       { path: "item/:id", element: <Item /> },
       { path: "myPage", element: <MyPage /> },
       { path: "myPage/myWishList", element: <MyPage /> },
+      { path: "myPage/order/cart", element: <MyPage /> },
       { path: "myPage/order/new", element: <MyPage /> },
     ],
   },
 ]);
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
