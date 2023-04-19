75af4fef00a1afb14afbd05436349824a59d09c5
heywon0909 <hw7319@naver.com>
2023-04-19 23:07:55 +0900

    staleTime 제거

--- src/pages/Item.jsx
+++ src/pages/Item.jsx
@@ -6,9 +6,7 @@ import { useQuery } from "@tanstack/react-query";
 export default function Item() {
   const { id } = useParams();
   const { shop } = useShopApi();
-  const { isLoading, data } = useQuery(["itemDetail"], () => shop.getItem(id), {
-    staleTime: 1000 * 60 * 1,
-  });
+  const { isLoading, data } = useQuery(["itemDetail"], () => shop.getItem(id));
   return (
     <>
       <ItemDetail id={id} data={data} isLoading={isLoading} />
