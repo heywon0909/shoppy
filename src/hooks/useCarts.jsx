0e89a4f44fb8812c3746db5354995907ee1fe535
hw7319 <hw7319@naver.com>
2023-05-04 11:02:27 +0900

    useCart 커스텀 훅 구현 완료..

--- src/hooks/useCarts.jsx
+++ src/hooks/useCarts.jsx
@@ -3,18 +3,18 @@ import { addNewCart, getCarts, removeItem } from "api/firebase";
 import { useAuthApi } from "context/AuthContext";
 export default function useCarts() {
   const { uid } = useAuthApi();
-  console.log("uid", uid);
+
   const queryClient = useQueryClient();
-  const cartsQuery = useQuery(["cartItems"], () => getCarts(uid), {
+  const cartsQuery = useQuery(["cartItems", uid || ""], () => getCarts(uid), {
     enabled: !!uid,
   });
   const addNewCartItem = useMutation((item) => addNewCart(item, uid), {
-    onSuccess: () => queryClient.invalidateQueries(["cartItems"]),
+    onSuccess: () => queryClient.invalidateQueries(["cartItems", uid]),
   });
   const removeCartItem = useMutation(
     (itemId) => removeItem("cart", itemId, uid),
     {
-      onSuccess: () => queryClient.invalidateQueries(["cartItems"]),
+      onSuccess: () => queryClient.invalidateQueries(["cartItems", uid]),
     }
   );
   return { cartsQuery, addNewCartItem, removeCartItem };
